ca05dab1bbdd25ed14ff0318da7e42381b5e304e
whiteRocket <aljaz.podgornik@t-2.net>
2018-12-05 14:03:22 +0100

    moving camera and player

---
+++ map_generator/Assets/scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    int[] spodnjaLeva;
+    int[] zgornjaDesna;
+    // Use this for initialization
+    void Start () {
+        spodnjaLeva = new int[2];
+        zgornjaDesna = new int[2];
+
+        spodnjaLeva[0] = -5;
+        spodnjaLeva[1] = -2;
+
+        zgornjaDesna[0] = 5;
+        zgornjaDesna[1] = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (player.GetComponent<Transform>().position.x > zgornjaDesna[0])
+        {
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + 10.0f, 
+                GetComponent<Transform>().position.y, GetComponent<Transform>().position.z);
+
+            zgornjaDesna[0] += 10;
+            spodnjaLeva[0] += 10;
+        }
+
+        if (player.GetComponent<Transform>().position.x < spodnjaLeva[0])
+        {
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - 10.0f,
+                GetComponent<Transform>().position.y, GetComponent<Transform>().position.z);
+
+            zgornjaDesna[0] -= 10;
+            spodnjaLeva[0] -= 10;
+        }
+    }
+}
--- map_generator/Assets/scripts/PlayerScript.cs
+++ map_generator/Assets/scripts/PlayerScript.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	// Update is called once per 
+	void Update ()
+
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x -1.0f, GetComponent<Transform>().position.y); 
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x + 1.0f, GetComponent<Transform>().position.y);
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y + 1.0f);
+        }
+
+        else if (Input.GetKeyDown("down"))
+        {
+            GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y - 1.0f);
+        }
+    }
+
 }
